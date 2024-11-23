9b0084aa5c15e87f9e3fe177b63c1466034653d0
Muhaiminul coding club <mahi.aideveloper@gmail.com>
2024-11-23 20:25:29 +0600

    React basic hook

---
+++ src/hooks/CardCounter.jsx
@@ -0,0 +1,23 @@
+import { Button,  Typography } from '@mui/material'
+import { Container, Stack } from '@mui/system'
+import React, { useState} from 'react'
+
+function CardCounter() {
+  const Increasement = () => {setCount(count+1);};
+  const Decreasement = () => {setCount(count -1);};
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+        <Container align='center'>
+            <Typography variant='h1' sx={{fontSize:"3rem", my:'5rem'}}>Card counter!</Typography>
+            <Typography variant='p' sx={{fontSize:"2rem"}} gutterBottom>Count:{count}</Typography>
+              <Stack direction={"row"} my={"2rem"} mx={"30rem"} spacing={3}>
+                <Button variant='contained' color='success' onClick={Increasement}>Increase</Button>
+                <Button variant='contained' color='error' onClick={Decreasement}>Decrease</Button>
+              </Stack>
+        </Container>
+    </div>
+  )
+}
+
+export default CardCounter
